64a6f09ab9f21de0c9316a081a10a65050c99051
Jamie Doyle <jdoyle@flexera.com>
2019-07-27 09:14:43 +0100

    Move to one design file

--- design/design.go
+++ design/design.go
@@ -5,6 +5,20 @@ import (
 	. "goa.design/goa/dsl"
 )
 
+var Table = Type("Table", func() {
+	Description("Describes a table")
+	Field(1, "table_id", String, "A table ID", func() {
+		Example("some-table-id")
+	})
+	Required("table_id")
+})
+
+var TablePayload = ResultType("TablePayload", func() {
+	Extend(Table)
+	Field(3, "description", String, "Describes the table being returned")
+})
+
+
 var _ = API("Table Info Service", func() {
 	Title("Table Info Service")
 	Description("Goa test service")
